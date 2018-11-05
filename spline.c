bd4fa002818b6994be3b2e84ac0c4470d4ef9097
MaJangCowBoy <44767143+MaJangCowBoy@users.noreply.github.com>
2018-11-05 17:59:18 +0900

    Update spline.c

--- spline.c
+++ spline.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
+#include "spline.h"
+#include <stdlib.h>
+#include <math.h>
 
 int main() {
+ 
+  int i,j,k;
+  FILE *fp;
+  double x,y,z;
+  
+  fp = fopen("input.txt","r");
+  
+  if(fp == NULL) { printf("error open file\n"); exit(1); }
 
+  
+  
+  return 0;
 }
