695e4be57ffd8638577c4d3e1f04853f1922daca
MaJangCowBoy <44767143+MaJangCowBoy@users.noreply.github.com>
2018-11-05 18:09:16 +0900

    Update splint3d.c

--- splint3d.c
+++ splint3d.c
@@ -9,14 +9,23 @@
 int main() {
  
   int i,j,k;
+  int a,b,c;
   FILE *fp;
-  double _val_x,_val_y,_val_z;
+  double _Val_x,_Val_y,_Val_z;
+ 
+  double 
+  double _Coeff[64];
   
   fp = fopen("input.txt","r");
   
   if(fp == NULL) { printf("error open file\n"); exit(1); }
 
-  
+  fscanf();
+ 
+ for(i=0;i<64;i++)
+  for(j=0;j<64;j++) {
+    
+  }
   
   return 0;
 }
