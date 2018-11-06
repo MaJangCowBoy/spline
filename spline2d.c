2c65b7cce92f99e646699abde42d85a4cac44516
MaJangCowBoy <44767143+MaJangCowBoy@users.noreply.github.com>
2018-11-06 13:07:17 +0900

    Create spline2d.c

---
+++ spline2d.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "spline.h"
+#include <stdlib.h>
+#include <math.h>
+
+#define _NL 2
+
+int main() {
+  
+  int i,j,k;
+  int a,b,c;
+  int N,M;
+  double _Val_x,_Val_y,_Val_z;
+  double _Coeff_a[16][_NL][_NL];    /* Coefficient vector */
+  double _tmp_BC[16];         /* Boundary condition */
+  FILE *fp;
+  
+  fp = fopen("something.txt","r");
+
+  for(N=0;N<_NL;N++) 
+    for(M=0;M<_NL;M++) {
+
+      for(j=0;j<16;j++) {
+        fscanf(fp,"%lf",&_tmp_BC[j]); 
+      }
+    
+      for(i=0;i<16;i++) {
+        _Coeff_a[i][N][M] = 0.0;
+      }
+    
+      for(i=0;i<16;i++) {
+          _Coeff_a[i][N][M] += _C2[i][j]*_tmp_BC[j];
+      }
+    }
+    
+  return 0;
+}
