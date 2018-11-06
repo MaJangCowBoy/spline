2384e071ca969a6ec9ece6abf7e1389c1db10d00
MaJangCowBoy <44767143+MaJangCowBoy@users.noreply.github.com>
2018-11-06 21:58:21 +0900

    Update BC_maker1D.c
    fprintf to "something.txt"
    >> (derivative)*(delta_X) to (derivative)

--- BC_maker1D.c
+++ BC_maker1D.c
@@ -2,27 +2,49 @@
 #include <stdlib.h>
 #include <math.h>
 
+#ifndef _INTV
+#define _INTV 0.1
+#endif
+
 int main() {
 
   int i,j,k;
+  int NL;
   double _Val_x[11], _Val_y[11], _Val_z[11];
   FILE *fp;
+	
+  fp = fopen("nothing.txt","r");
+
+  fscanf(fp,"%d",&NL);
+	
+  double coeff[NL];
   
+  for(i=0;i<NL;i++) {
+    fscanf(fp,"%lf",&coeff[i]);  
+  }
+	
   for(i=0;i<11;i++) {
-    _Val_x[i] = i * 0.2 * M_PI;
-    _Val_y[i] = sin(_Val_x);
-    _Val_z[i] = cos(_Val_x);
+    _Val_x[i] = _INTV * i;
+    _Val_y[i] = 0.0;
+    _Val_z[i] = 0.0;
+    for(j=0;j<NL;j++) {
+      _Val_y[i] += cos(M_PI*j*_Val_x[i]);
+      _Val_z[i] += -sin*M_PI*j*sin(M_PI*j*_Val_x[i]);
+    }
   }
   
-  fp = fopen("something.txt","w");
-  
-  fprintf(fp,"0.1\n");
+  fp = fopen("something.txt","w"); 
+
+  fprintf(fp,"%lf\n",_INTV);
   
   for(i=0;i<10;i++) {
-    fprintf(fp,"%lf\t%lf\t%lf\t%lf\n",
-            _Val_y[i],_Val_y[i+1],
-            (0.2*M_PI*_Val_z[i]),(0.2*M_PI*_Val_z[i+1]));
+    fprintf(fp,"%lf\t",_Val_y[i]);
+    fprintf(fp,"%lf\t",_Val_y[i+1]);
+    fprintf(fp,"%lf\t",_Val_z[i]);
+    fprintf(fp,"%lf\n",_Val_z[i+1]);
   }
+	 
+  fclose(fp);
     
   return 0;
 }
