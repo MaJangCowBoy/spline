8df0924effba06d439f5a10ef3f5e1fe5d280ec8
MaJangCowBoy <44767143+MaJangCowBoy@users.noreply.github.com>
2018-11-06 23:29:16 +0900

    Update splint2d.c

--- splint2d.c
+++ splint2d.c
@@ -3,13 +3,13 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define _NL 10
-
 int main() {
   
   int i,j,k;
   int a,b,c;
   int N,M;
+  int _NL;
+  double _delX,_delY;
   double _Val_x,_Val_y,_Val_z;
   double _Coeff_a[16][_NL][_NL];    /* Coefficient vector */
   double _tmp_BC[16];         /* Boundary condition */
@@ -17,25 +17,53 @@ int main() {
   
   fp = fopen("something.txt","r");
 
+  fscanf(fp,"%d",&_NL);
+  fscanf(fp,"%lf",&_delX);
+  fscanf(fp,"%lf",&_delY);
+  
   for(N=0;N<_NL;N++) 
     for(M=0;M<_NL;M++) {
-
-      for(j=0;j<16;j++) {
-        fscanf(fp,"%lf",&_tmp_BC[j]); 
+    
+      for(i=0;i<4;i++) {
+        fscanf(fp,"%lf",&_tmp_BC[i]); 
+      }
+      for(i=4;i<8;i++) {
+        fscanf(fp,"%lf",&_tmp_BC[i]);
+        _tmp_BC[i] *= _delX;
+      }
+      for(i=8;i<12;i++) {
+        fscanf(fp,"%lf",&_tmp_BC[i]);
+        _tmp_BC[i] *= _delY;
+      }
+      for(i=12;i<16;i++) {
+        fscanf(fp,"%lf",&_tmp_BC[i]);
+        _tmp_BC[i] *= _delX*_delY;
       }
     
       for(i=0;i<16;i++) {
         _Coeff_a[i][N][M] = 0.0;
       }
     
-      for(i=0;i<16;i++) {
+      for(i=0;i<16;i++) 
+        for(j=0;j<16;j++) {
           _Coeff_a[i][N][M] += _C2[i][j]*_tmp_BC[j];
-      }
+        }
     }
-    
+  
+  fclose(fp);
+  
   fp = fopen("something_else.txt","w");
   
-  for(N=0;N<_NL)
+  for(N=0;N<_NL;N++) 
+    for(M=0;M<_NL;M++) {
+      
+      for(i=0;i<16;i++) {
+        fprintf(fp,"%lf\t",_Coeff_a[0][N]);
+      }
+      fprintf(fp,"\n");
+  }
+  
+  fclose(fp);
   
   return 0;
 }
