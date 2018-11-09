35a180f766081a853f811fff78c77cef7bf52616
MaJangCowBoy <44767143+MaJangCowBoy@users.noreply.github.com>
2018-11-09 15:14:22 +0900

    Update Plot3D.c

--- Plot3D.c
+++ Plot3D.c
@@ -4,47 +4,56 @@
 
 int main() {
   int i,j,k;
+  int a,b,c;
   int N,M,L;
   FILE *fp;
   int _NL;
-  int Quot,Resd;
-  double _delX,_delY;
-  double coeff[16];
-  double _tmpX,_tmpY,_tmpZ;
+  int Resd,Quot1,Quot2;
+  double _delX,_delY,_delZ;
+  double coeff[64];
+  double _tmpX,_tmpY,_tmpZ,_tmpF;
 
   fp = fopen("something_else.txt","r");
   
   fscanf(fp,"%d",&_NL);
   fscanf(fp,"%lf",&_delX);
   fscanf(fp,"%lf",&_delY);
+  fscanf(fp,"%lf",&_delY);
   
   for(i=0;i<_NL;i++)
-    for(j=0;j<_NL;j++) {
-    
-      for(k=0;k<16;k++) {
-        fscanf(fp,"%lf",&coeff[k]);
-      }
-    
-      for(N=0;N<10;N++) 
-        for(M=0;M<10;M++) {
-        
-          _tmpX = N * 0.1;
-          _tmpY = M * 0.1;
-          
-          _tmpZ = 0.0;
-          for(L=0;L<16;L++) {
-            Quot = L/4;
-            Resd = L%4;
-            _tmpZ += coeff[k] * pow(_tmpX,Resd) * pow(_tmpY,Quot);
-          }
-        
-          _tmpX *= _delX;  _tmpX += i * _delX;
-          _tmpY *= _delY;  _tmpY += j * _delY;
-        
-          printf("%lf\t%lf\t%lf\n",_tmpX,_tmpY,_tmpZ);
+    for(j=0;j<_NL;j++) 
+      for(k=0;k<_NL;k++) {
+      
+        for(a=0;a<64;a++) {
+          fscanf(fp,"%lf",&coeff[a]);
         }
-    }  
-
+      
+        for(N=0;N<10;N++) 
+          for(M=0;M<10;M++) 
+            for(L=0;L<10;L++) {
+            
+              _tmpX = N * 0.1;
+              _tmpY = M * 0.1;
+              _tmpZ = L * 0.1;
+              
+              for(a=0;a<64;a++) {
+                
+                Resd  = a % 4;
+                Quot1 = a % 16;  Quot1 /= 4;
+                Quot2 = a / 16;
+                
+                _tmpF += coeff[a] * pow(_tmpX,Resd) * pow(_tmpY,Quot1) * pow(_tmpZ,Quot2);
+              }
+            
+              _tmpX *= _delX;  _tmpX += i * _delX;
+              _tmpY *= _delY;  _tmpY += j * _delY;
+              _tmpZ *= _delZ;  _tmpZ += k * _delZ;
+            
+              printf("%lf\t%lf\t%lf\t%lf\n",_tmpX,_tmpY,_tmpZ,_tmpF);
+            }
+        
+      }  
+      
   fclose(fp);
 
   return 0;
