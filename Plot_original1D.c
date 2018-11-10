6e043eb3a1a3d319bdc228e10a25c5598127fd82
MaJangCowBoy <44767143+MaJangCowBoy@users.noreply.github.com>
2018-11-10 21:21:24 +0900

    Update Plot_original1D.c

--- Plot_original1D.c
+++ Plot_original1D.c
@@ -7,7 +7,7 @@ int main() {
   int i,j,k;
   int N,M,L;
   int a,b,c;
-  int _NL;
+  int _NL,_NX;
   double _ValX,_ValY,_ValZ,_ValF;
   double _delX,_delY,_delZ;
   double _tmpX,_tmpY,_tmpZ,_tmpF;
@@ -15,25 +15,43 @@ int main() {
   
   fp = fopen("nothing.txt","r");
   
-  fscanf(fp,"%d",_NL);
+  fscanf(fp,"%d",&_NL);
   
   double _Coeff[_NL];
   
   for(i=0;i<_NL;i++) {
   
-    fscanf(fp,"%lf",_Coeff[i]);
+    fscanf(fp,"%lf",&_Coeff[i]);
   }
   
   fclose(fp);
   
-  fp = fopen("something","r");
+  fp = fopen("something.txt","r");
   
-  fscanf(fp,"%d",_NL);
-  fscanf(fp,"%lf",_delX);
+  fscanf(fp,"%d",&_NX);
+  fscanf(fp,"%lf",&_delX);
   
   fclose(fp);
   
-  for(i=0;i<_)
+  fp = fopen("original.txt","w");
+  
+  for(i=0;i<_NX;i++) {
+  
+    for(j=0;j<10;j++) {
+    
+      _ValX = i * _delX + 0.1 * j * delX;
+      
+      _ValY  = 0.0;
+      for(k=0;k<_NL;k++) {
+      
+        _ValY += _Coeff[k] * cos(k*M_PI*_ValX);
+      }
+      
+      fprintf(fp,"%lf\t%lf\n",_ValX,_ValY);
+    }
+  }
+  
+  fclose(fp);
 
   return 0;
 }
