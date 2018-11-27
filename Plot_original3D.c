ece006e12e99f3bd241d43f74ae235f149fa17e4
MaJangCowBoy <44767143+MaJangCowBoy@users.noreply.github.com>
2018-11-27 16:11:08 +0900

    Update Plot_original3D.c

--- Plot_original3D.c
+++ Plot_original3D.c
@@ -7,8 +7,7 @@ int main() {
   int i,j,k;
   int N,M,L;
   int a,b,c;
-  int _NL,_NL1,_NL2;
-  int resd,quot1,quot2;
+  int _NL,_NR;
   double _ValX,_ValY,_ValZ,_ValF;
   double _delX,_delY,_delZ;
   double _tmpX,_tmpY,_tmpZ,_tmpF;
@@ -17,20 +16,20 @@ int main() {
   fp = fopen("nothing.txt","r");
   
   fscanf(fp,"%d",&_NL);
-  _NL1 = _NL*_NL*_NL;
   
-  double _Coeff[_NL1];
+  double _Coeff[_NL][_NL][_NL];
   
-  for(i=0;i<(_NL1);i++) {
-  
-    fscanf(fp,"%lf",&_Coeff[i]);
-  }
+  for(i=0;i<_NL;i++)
+    for(j=0;j<_NL;j++)
+      for(k=0;k<_NL;k++) {
+        fscanf(fp,"%lf",&_Coeff[i][j][k]);
+      }
   
   fclose(fp);
   
   fp = fopen("something.txt","r");
   
-  fscanf(fp,"%d",&_NL2);
+  fscanf(fp,"%d",&_NR);
   fscanf(fp,"%lf",&_delX);
   fscanf(fp,"%lf",&_delY);
   fscanf(fp,"%lf",&_delZ);
@@ -39,35 +38,32 @@ int main() {
   
   fp = fopen("original.txt","w");
   
-  for(i=0;i<_NL2;i++)
-    for(j=0;j<_NL2;j++)
-      for(k=0;k<_NL2;k++) {
+  for(i=0;i<_NR;i++)
+    for(j=0;j<_NR;j++)
+      for(k=0;k<_NR;k++) {
       
-        for(N=0;N<10;N++)
-          for(M=0;M<10;M++)
-            for(L=0;L<10;L++) {
+        for(N=0;N<2;N++)
+          for(M=0;M<2;M++)
+            for(L=0;L<2;L++) {
             
-              _ValX = i * _delX + 0.1 * N * _delX;
-              _ValY = j * _delY + 0.1 * M * _delY;
-              _ValZ = k * _delZ + 0.1 * L * _delZ;
+              _ValX = i * _delX + 0.5 * N * _delX;
+              _ValY = j * _delY + 0.5 * M * _delY;
+              _ValZ = k * _delZ + 0.5 * L * _delZ;
             
               _ValF = 0.0;
-              for(a=0;a<(_NL1);a++) {
-            
-                resd  = a % _NL;
-                quot1 = a % (_NL*_NL);  quot1 = quot1 / _NL;
-                quot2 = a / (_NL*_NL); 
-                _ValF += _Coeff[a] 
-                        * cos(resd*M_PI*_ValX)
-                        * cos(quot1*M_PI*_ValY)
-                        * cos(quot2*M_PI*_ValZ);
-              }
-          
+              for(a=0;a<_NL;a++) 
+                for(b=0;b<_NL;b++)    
+                  for(c=0;c<_NL;c++) {
+                      _ValF += _Coeff[a][b][c]
+                             * sin( (a+1)*M_PI*_ValX )
+                             * sin( (b+1)*M_PI*_ValY )
+                             * sin( (c+1)*M_PI*_ValZ );
+                    }
+                    
               fprintf(fp,"%lf\t%lf\t%lf\t%lf\n",_ValX,_ValY,_ValZ,_ValF);
             }
       }
-      
-  
+       
   fclose(fp);
 
   return 0;
