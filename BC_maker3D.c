c3fd3c81c6e747d946c613bdb30fc1efb640b350
MaJangCowBoy <44767143+MaJangCowBoy@users.noreply.github.com>
2018-11-28 17:10:48 +0900

    Update BC_maker3D.c

--- BC_maker3D.c
+++ BC_maker3D.c
@@ -3,19 +3,19 @@
 #include <math.h>
 
 #ifndef _INTVX
-#define _INTVX 0.1
+#define _INTVX 0.05
 #endif
 
 #ifndef _INTVY
-#define _INTVY 0.1
+#define _INTVY 0.05
 #endif
 
 #ifndef _INTVZ
-#define _INTVZ 0.1
+#define _INTVZ 0.05
 #endif
 
 #ifndef _NPOINT
-#define _NPOINT 11
+#define _NPOINT 21
 #endif
 
 int main() {
@@ -24,7 +24,10 @@ int main() {
   int N,M,L;
   int a,b,c;
   int NL;
-  double Fc;
+  double _FCx, _FCxp;
+  double _FCy, _FCyp;
+  double _FCz, _FCzp;
+  double _FU, _FUx, _FUy, _FUz, _FUxy, _FUyz, _FUxz, _FUxyz;
   double _ValX, _ValY, _ValZ;
   double _ValF[_NPOINT][_NPOINT][_NPOINT];
   double _ValFx[_NPOINT][_NPOINT][_NPOINT], _ValFy[_NPOINT][_NPOINT][_NPOINT], _ValFz[_NPOINT][_NPOINT][_NPOINT];
@@ -54,157 +57,180 @@ int main() {
         _ValY = _INTVY * j;
         _ValZ = _INTVZ * k;
         
-        _ValF[i][j][k] = 0.0; 
-        _ValFx[i][j][k] = 0.0; _ValFy[i][j][k] = 0.0; _ValFz[i][j][k] = 0.0;
-        _ValFxy[i][j][k] = 0.0; _ValFxz[i][j][k] = 0.0; _ValFyz[i][j][k] = 0.0;
-        _ValFxyz[i][j][k] = 0.0;
+        
+        
+        _FU = 0.0;
+        _FUx = 0.0; _FUy = 0.0; _FUz = 0.0;
+        _FUxy = 0.0; _FUxz = 0.0; _FUyz = 0.0;
+        _FUxyz = 0.0;
 	    
-        for(N = 0; N < NL; N++) 
-          for(M = 0; M < NL; M++)  
+        for(N = 0; N < NL; N++)
+          for(M = 0; M < NL; M++)
             for(L = 0; L < NL; L++) {
 		
-              _ValF[i][j][k] += coeff[N][M][L]
-                              * sin( (N+1)*M_PI*_ValX )
-                              * sin( (M+1)*M_PI*_ValY )
-                              * sin( (L+1)*M_PI*_ValZ );
+              _FU += coeff[N][M][L]
+                   * sin( (N+1)*M_PI*_ValX )
+                   * sin( (M+1)*M_PI*_ValY )
+                   * sin( (L+1)*M_PI*_ValZ );
                                 
-              _ValFx[i][j][k] += (N+1) * M_PI * coeff[N][M][L]
-                               * cos( (N+1)*M_PI*_ValX )
-                               * sin( (M+1)*M_PI*_ValY )
-                               * sin( (L+1)*M_PI*_ValZ );
+              _FUx += (N+1) * M_PI * coeff[N][M][L]
+                    * cos( (N+1)*M_PI*_ValX )
+                    * sin( (M+1)*M_PI*_ValY )
+                    * sin( (L+1)*M_PI*_ValZ );
 
-              _ValFy[i][j][k] += (M+1) * M_PI * coeff[N][M][L]
-                               * sin( (N+1)*M_PI*_ValX )
-                               * cos( (M+1)*M_PI*_ValY )
-                               * sin( (L+1)*M_PI*_ValZ );
+              _FUy += (M+1) * M_PI * coeff[N][M][L]
+                    * sin( (N+1)*M_PI*_ValX )
+                    * cos( (M+1)*M_PI*_ValY )
+                    * sin( (L+1)*M_PI*_ValZ );
                                  
-              _ValFz[i][j][k] += (L+1) * M_PI * coeff[N][M][L]
-                               * sin( (N+1)*M_PI*_ValX )
-                               * sin( (M+1)*M_PI*_ValY )
-                               * cos( (L+1)*M_PI*_ValZ );
+              _FUz += (L+1) * M_PI * coeff[N][M][L]
+                    * sin( (N+1)*M_PI*_ValX )
+                    * sin( (M+1)*M_PI*_ValY )
+                    * cos( (L+1)*M_PI*_ValZ );
                                  
-              _ValFxy[i][j][k] += (N+1) * (M+1) * M_PI * M_PI
-                                * coeff[N][M][L]
-                                * cos( (N+1)*M_PI*_ValX )
-                                * cos( (M+1)*M_PI*_ValY )
-                                * sin( (L+1)*M_PI*_ValZ );
+              _FUxy += (N+1) * (M+1) * M_PI * M_PI
+                     * coeff[N][M][L]
+                     * cos( (N+1)*M_PI*_ValX )
+                     * cos( (M+1)*M_PI*_ValY )
+                     * sin( (L+1)*M_PI*_ValZ );
                                   
-              _ValFxz[i][j][k] += (N+1) * (L+1) * M_PI * M_PI
-                                * coeff[N][M][L]
-                                * cos( (N+1)*M_PI*_ValX )
-                                * sin( (M+1)*M_PI*_ValY )
-                                * cos( (L+1)*M_PI*_ValZ );
+              _FUxz += (N+1) * (L+1) * M_PI * M_PI
+                     * coeff[N][M][L]
+                     * cos( (N+1)*M_PI*_ValX )
+                     * sin( (M+1)*M_PI*_ValY )
+                     * cos( (L+1)*M_PI*_ValZ );
                                   
-              _ValFyz[i][j][k] += (M+1) * (L+1) * M_PI * M_PI
-                                * coeff[N][M][L]
-                                * sin( (N+1)*M_PI*_ValX )
-                                * cos( (M+1)*M_PI*_ValY )
-                                * cos( (L+1)*M_PI*_ValZ );
+              _FUyz += (M+1) * (L+1) * M_PI * M_PI
+                     * coeff[N][M][L]
+                     * sin( (N+1)*M_PI*_ValX )
+                     * cos( (M+1)*M_PI*_ValY )
+                     * cos( (L+1)*M_PI*_ValZ );
                                   
-              _ValFxyz[i][j][k] += (N+1) * (M+1) * (L+1)
-                                 * M_PI * M_PI * M_PI
-                                 * coeff[N][M][L]
-                                 * cos( (N+1)*M_PI*_ValX )
-                                 * cos( (M+1)*M_PI*_ValY )
-                                 * cos( (L+1)*M_PI*_ValZ );
+              _FUxyz += (N+1) * (M+1) * (L+1)
+                      * M_PI * M_PI * M_PI
+                      * coeff[N][M][L]
+                      * cos( (N+1)*M_PI*_ValX )
+                      * cos( (M+1)*M_PI*_ValY )
+                      * cos( (L+1)*M_PI*_ValZ );
             }
             
             if( _ValX < 0.1 ) {
-              Fc = ( 1 - cos(10*M_PI*_ValX) ) * 0.5;
-              _ValF[i][j][k] *= Fc;
-              _ValFx[i][j][k] *= Fc;  _ValFy[i][j][k] *= Fc;  _ValFz[i][j][k] *= Fc;
-              _ValFxy[i][j][k] *= Fc;  _ValFxz[i][j][k] *= Fc;  _ValFyz[i][j][k] *= Fc;
-              _ValFxyz[i][j][k] *= Fc;
-              
+              _FCx = ( ( 1 - cos(10*M_PI*_ValX) ) * 0.5 );
+              _FCxp = ( 5 * M_PI * sin(10*M_PI*_ValX) );
             } else if( _ValX > 0.9 ) {
-              Fc = ( 1 + cos(10*M_PI*(_ValX-0.9)) ) * 0.5;
-              _ValF[i][j][k] *= Fc;
-              _ValFx[i][j][k] *= Fc;  _ValFy[i][j][k] *= Fc;  _ValFz[i][j][k] *= Fc;
-              _ValFxy[i][j][k] *= Fc;  _ValFxz[i][j][k] *= Fc;  _ValFyz[i][j][k] *= Fc;
-              _ValFxyz[i][j][k] *= Fc;
-              
-            } else {    }
+              _FCx = ( ( 1 + cos(10*M_PI*(_ValX-0.9)) ) * 0.5 );
+              _FCxp = ( -5 * M_PI * sin(10*M_PI*(_ValX-0.9)) );
+            } else {
+              _FCx = 1.0;
+              _FCxp = 0.0;
+            }
             
-            if ( _ValY < 0.1 ) {
-              Fc = ( 1 - cos(10*M_PI*_ValY) ) * 0.5;
-              _ValF[i][j][k] *= Fc;
-              _ValFx[i][j][k] *= Fc;  _ValFy[i][j][k] *= Fc;  _ValFz[i][j][k] *= Fc;
-              _ValFxy[i][j][k] *= Fc;  _ValFxz[i][j][k] *= Fc;  _ValFyz[i][j][k] *= Fc;
-              _ValFxyz[i][j][k] *= Fc;
-              
+            if( _ValY < 0.1 ) {
+              _FCy = ( ( 1 - cos(10*M_PI*_ValY) ) * 0.5 );
+              _FCyp = ( 5 * M_PI * sin(10*M_PI*_ValY) );
             } else if( _ValY > 0.9 ) {
-              Fc = ( 1 + cos(10*M_PI*(_ValY-0.9)) ) * 0.5;
-              _ValF[i][j][k] *= Fc;
-              _ValFx[i][j][k] *= Fc;  _ValFy[i][j][k] *= Fc;  _ValFz[i][j][k] *= Fc;
-              _ValFxy[i][j][k] *= Fc;  _ValFxz[i][j][k] *= Fc;  _ValFyz[i][j][k] *= Fc;
-              _ValFxyz[i][j][k] *= Fc;
-              
-            } else {    }
+              _FCy = ( ( 1 + cos(10*M_PI*(_ValY-0.9)) ) * 0.5 );
+              _FCyp = ( -5 * M_PI * sin(10*M_PI*(_ValY-0.9)) );
+            } else {
+              _FCy = 1.0;
+              _FCyp = 0.0;
+            }
             
-            if ( _ValZ < 0.1 ) {
-              Fc = ( 1 - cos(10*M_PI*_ValZ) ) * 0.5;
-              _ValF[i][j][k] *= Fc;
-              _ValFx[i][j][k] *= Fc;  _ValFy[i][j][k] *= Fc;  _ValFz[i][j][k] *= Fc;
-              _ValFxy[i][j][k] *= Fc;  _ValFxz[i][j][k] *= Fc;  _ValFyz[i][j][k] *= Fc;
-              _ValFxyz[i][j][k] *= Fc;
-              
+            if( _ValZ < 0.1 ) {
+              _FCz = ( ( 1 - cos(10*M_PI*_ValZ) ) * 0.5 );
+              _FCzp = ( 5 * M_PI * sin(10*M_PI*_ValZ) );              
             } else if( _ValZ > 0.9 ) {
-              Fc = ( 1 + cos(10*M_PI*(_ValZ-0.9)) ) * 0.5;
-              _ValF[i][j][k] *= Fc;
-              _ValFx[i][j][k] *= Fc;  _ValFy[i][j][k] *= Fc;  _ValFz[i][j][k] *= Fc;
-              _ValFxy[i][j][k] *= Fc;  _ValFxz[i][j][k] *= Fc;  _ValFyz[i][j][k] *= Fc;
-              _ValFxyz[i][j][k] *= Fc;
-              
-            } else {    }
+              _FCz = ( ( 1 + cos(10*M_PI*(_ValZ-0.9)) ) * 0.5 );
+              _FCzp = ( -5 * M_PI * sin(10*M_PI*(_ValZ-0.9)) );
+            } else {
+              _FCz = 1.0;
+              _FCzp = 0.0;
+            }
+
+            _ValF[i][j][k] = _FU * _FCx * _FCy * _FCz;
+
+            _ValFx[i][j][k] = _FUx * _FCx * _FCy * _FCz
+                            + _FU * _FCxp * _FCy * _FCz;
+                            
+            _ValFy[i][j][k] = _FUy * _FCx * _FCy * _FCz
+                            + _FU * _FCx * _FCyp * _FCz;
+                            
+            _ValFz[i][j][k] = _FUz * _FCx * _FCy * _FCz
+                            + _FU * _FCx * _FCy * _FCzp;
+                            
+            _ValFxy[i][j][k] = _FUxy * _FCx * _FCy * _FCz
+                             + _FUx * _FCx * _FCyp * _FCz
+                             + _FUy * _FCxp * _FCy * _FCz
+                             + _FU * _FCxp * _FCyp * _FCz;
+                             
+            _ValFxz[i][j][k] = _FUxz * _FCx * _FCy * _FCz
+                             + _FUx * _FCx * _FCy * _FCzp
+                             + _FUz * _FCxp * _FCy * _FCz
+                             + _FU * _FCxp * _FCy * _FCzp;
+                             
+            _ValFyz[i][j][k] = _FUyz * _FCx * _FCy * _FCz
+                             + _FUy * _FCx * _FCy * _FCzp
+                             + _FUz * _FCx * _FCyp * _FCz
+                             + _FU * _FCx * _FCyp * _FCzp;
+            
+            _ValFxyz[i][j][k] = _FUxyz* _FCx * _FCy * _FCz
+                              + _FUxy* _FCx * _FCy * _FCzp
+                              + _FUxz* _FCx * _FCyp * _FCz
+                              + _FUyz* _FCxp * _FCy * _FCz
+                              + _FUx* _FCx * _FCyp * _FCzp              
+                              + _FUy* _FCxp * _FCy * _FCzp
+                              + _FUz* _FCxp * _FCyp * _FCz
+                              + _FU* _FCxp * _FCyp * _FCzp;
+            
       }
   
   fp = fopen("something.txt","w"); 
 
   fprintf(fp,"%d\n",(_NPOINT-1));
-  fprintf(fp,"%lf\t%lf\t%lf\n",_INTVX,_INTVY,_INTVZ);
+  fprintf(fp,"%.15e\t%.15e\t%.15e\n",_INTVX,_INTVY,_INTVZ);
   
   for(i=0;i<(_NPOINT-1);i++) 
     for(j=0;j<(_NPOINT-1);j++) 
       for(k=0;k<(_NPOINT-1);k++) {
-        fprintf(fp,"%lf\t",_ValF[i][j][k]);  fprintf(fp,"%lf\t",_ValF[i+1][j][k]);
-        fprintf(fp,"%lf\t",_ValF[i][j+1][k]);  fprintf(fp,"%lf\t",_ValF[i+1][j+1][k]);
-        fprintf(fp,"%lf\t",_ValF[i][j][k+1]);  fprintf(fp,"%lf\t",_ValF[i+1][j][k+1]);
-        fprintf(fp,"%lf\t",_ValF[i][j+1][k+1]);  fprintf(fp,"%lf\t",_ValF[i+1][j+1][k+1]);
+        fprintf(fp,"%.15e\t",_ValF[i][j][k]);  fprintf(fp,"%.15e\t",_ValF[i+1][j][k]);
+        fprintf(fp,"%.15e\t",_ValF[i][j+1][k]);  fprintf(fp,"%.15e\t",_ValF[i+1][j+1][k]);
+        fprintf(fp,"%.15e\t",_ValF[i][j][k+1]);  fprintf(fp,"%.15e\t",_ValF[i+1][j][k+1]);
+        fprintf(fp,"%.15e\t",_ValF[i][j+1][k+1]);  fprintf(fp,"%.15e\t",_ValF[i+1][j+1][k+1]);
       
-        fprintf(fp,"%lf\t",_ValFx[i][j][k]);  fprintf(fp,"%lf\t",_ValFx[i+1][j][k]);
-        fprintf(fp,"%lf\t",_ValFx[i][j+1][k]);  fprintf(fp,"%lf\t",_ValFx[i+1][j+1][k]);
-        fprintf(fp,"%lf\t",_ValFx[i][j][k+1]);  fprintf(fp,"%lf\t",_ValFx[i+1][j][k+1]);
-        fprintf(fp,"%lf\t",_ValFx[i][j+1][k+1]);  fprintf(fp,"%lf\t",_ValFx[i+1][j+1][k+1]);
+        fprintf(fp,"%.15e\t",_ValFx[i][j][k]);  fprintf(fp,"%.15e\t",_ValFx[i+1][j][k]);
+        fprintf(fp,"%.15e\t",_ValFx[i][j+1][k]);  fprintf(fp,"%.15e\t",_ValFx[i+1][j+1][k]);
+        fprintf(fp,"%.15e\t",_ValFx[i][j][k+1]);  fprintf(fp,"%.15e\t",_ValFx[i+1][j][k+1]);
+        fprintf(fp,"%.15e\t",_ValFx[i][j+1][k+1]);  fprintf(fp,"%.15e\t",_ValFx[i+1][j+1][k+1]);
         
-        fprintf(fp,"%lf\t",_ValFy[i][j][k]);  fprintf(fp,"%lf\t",_ValFy[i+1][j][k]);
-        fprintf(fp,"%lf\t",_ValFy[i][j+1][k]);  fprintf(fp,"%lf\t",_ValFy[i+1][j+1][k]);
-        fprintf(fp,"%lf\t",_ValFy[i][j][k+1]);  fprintf(fp,"%lf\t",_ValFy[i+1][j][k+1]);
-        fprintf(fp,"%lf\t",_ValFy[i][j+1][k+1]);  fprintf(fp,"%lf\t",_ValFy[i+1][j+1][k+1]);
+        fprintf(fp,"%.15e\t",_ValFy[i][j][k]);  fprintf(fp,"%.15e\t",_ValFy[i+1][j][k]);
+        fprintf(fp,"%.15e\t",_ValFy[i][j+1][k]);  fprintf(fp,"%.15e\t",_ValFy[i+1][j+1][k]);
+        fprintf(fp,"%.15e\t",_ValFy[i][j][k+1]);  fprintf(fp,"%.15e\t",_ValFy[i+1][j][k+1]);
+        fprintf(fp,"%.15e\t",_ValFy[i][j+1][k+1]);  fprintf(fp,"%.15e\t",_ValFy[i+1][j+1][k+1]);
 
-        fprintf(fp,"%lf\t",_ValFz[i][j][k]);  fprintf(fp,"%lf\t",_ValFz[i+1][j][k]);
-        fprintf(fp,"%lf\t",_ValFz[i][j+1][k]);  fprintf(fp,"%lf\t",_ValFz[i+1][j+1][k]);
-        fprintf(fp,"%lf\t",_ValFz[i][j][k+1]);  fprintf(fp,"%lf\t",_ValFz[i+1][j][k+1]);
-        fprintf(fp,"%lf\t",_ValFz[i][j+1][k+1]);  fprintf(fp,"%lf\t",_ValFz[i+1][j+1][k+1]);
+        fprintf(fp,"%.15e\t",_ValFz[i][j][k]);  fprintf(fp,"%.15e\t",_ValFz[i+1][j][k]);
+        fprintf(fp,"%.15e\t",_ValFz[i][j+1][k]);  fprintf(fp,"%.15e\t",_ValFz[i+1][j+1][k]);
+        fprintf(fp,"%.15e\t",_ValFz[i][j][k+1]);  fprintf(fp,"%.15e\t",_ValFz[i+1][j][k+1]);
+        fprintf(fp,"%.15e\t",_ValFz[i][j+1][k+1]);  fprintf(fp,"%.15e\t",_ValFz[i+1][j+1][k+1]);
         
-        fprintf(fp,"%lf\t",_ValFxy[i][j][k]);  fprintf(fp,"%lf\t",_ValFxy[i+1][j][k]);
-        fprintf(fp,"%lf\t",_ValFxy[i][j+1][k]);  fprintf(fp,"%lf\t",_ValFxy[i+1][j+1][k]);
-        fprintf(fp,"%lf\t",_ValFxy[i][j][k+1]);  fprintf(fp,"%lf\t",_ValFxy[i+1][j][k+1]);
-        fprintf(fp,"%lf\t",_ValFxy[i][j+1][k+1]);  fprintf(fp,"%lf\t",_ValFxy[i+1][j+1][k+1]);
+        fprintf(fp,"%.15e\t",_ValFxy[i][j][k]);  fprintf(fp,"%.15e\t",_ValFxy[i+1][j][k]);
+        fprintf(fp,"%.15e\t",_ValFxy[i][j+1][k]);  fprintf(fp,"%.15e\t",_ValFxy[i+1][j+1][k]);
+        fprintf(fp,"%.15e\t",_ValFxy[i][j][k+1]);  fprintf(fp,"%.15e\t",_ValFxy[i+1][j][k+1]);
+        fprintf(fp,"%.15e\t",_ValFxy[i][j+1][k+1]);  fprintf(fp,"%.15e\t",_ValFxy[i+1][j+1][k+1]);
         
-        fprintf(fp,"%lf\t",_ValFxz[i][j][k]);  fprintf(fp,"%lf\t",_ValFxz[i+1][j][k]);
-        fprintf(fp,"%lf\t",_ValFxz[i][j+1][k]);  fprintf(fp,"%lf\t",_ValFxz[i+1][j+1][k]);
-        fprintf(fp,"%lf\t",_ValFxz[i][j][k+1]);  fprintf(fp,"%lf\t",_ValFxz[i+1][j][k+1]);
-        fprintf(fp,"%lf\t",_ValFxz[i][j+1][k+1]);  fprintf(fp,"%lf\t",_ValFxz[i+1][j+1][k+1]);        
+        fprintf(fp,"%.15e\t",_ValFxz[i][j][k]);  fprintf(fp,"%.15e\t",_ValFxz[i+1][j][k]);
+        fprintf(fp,"%.15e\t",_ValFxz[i][j+1][k]);  fprintf(fp,"%.15e\t",_ValFxz[i+1][j+1][k]);
+        fprintf(fp,"%.15e\t",_ValFxz[i][j][k+1]);  fprintf(fp,"%.15e\t",_ValFxz[i+1][j][k+1]);
+        fprintf(fp,"%.15e\t",_ValFxz[i][j+1][k+1]);  fprintf(fp,"%.15e\t",_ValFxz[i+1][j+1][k+1]);        
 
-        fprintf(fp,"%lf\t",_ValFyz[i][j][k]);  fprintf(fp,"%lf\t",_ValFyz[i+1][j][k]);
-        fprintf(fp,"%lf\t",_ValFyz[i][j+1][k]);  fprintf(fp,"%lf\t",_ValFyz[i+1][j+1][k]);
-        fprintf(fp,"%lf\t",_ValFyz[i][j][k+1]);  fprintf(fp,"%lf\t",_ValFyz[i+1][j][k+1]);
-        fprintf(fp,"%lf\t",_ValFyz[i][j+1][k+1]);  fprintf(fp,"%lf\t",_ValFyz[i+1][j+1][k+1]);
+        fprintf(fp,"%.15e\t",_ValFyz[i][j][k]);  fprintf(fp,"%.15e\t",_ValFyz[i+1][j][k]);
+        fprintf(fp,"%.15e\t",_ValFyz[i][j+1][k]);  fprintf(fp,"%.15e\t",_ValFyz[i+1][j+1][k]);
+        fprintf(fp,"%.15e\t",_ValFyz[i][j][k+1]);  fprintf(fp,"%.15e\t",_ValFyz[i+1][j][k+1]);
+        fprintf(fp,"%.15e\t",_ValFyz[i][j+1][k+1]);  fprintf(fp,"%.15e\t",_ValFyz[i+1][j+1][k+1]);
         
-        fprintf(fp,"%lf\t",_ValFxyz[i][j][k]);  fprintf(fp,"%lf\t",_ValFxyz[i+1][j][k]);
-        fprintf(fp,"%lf\t",_ValFxyz[i][j+1][k]);  fprintf(fp,"%lf\t",_ValFxyz[i+1][j+1][k]);
-        fprintf(fp,"%lf\t",_ValFxyz[i][j][k+1]);  fprintf(fp,"%lf\t",_ValFxyz[i+1][j][k+1]);
-        fprintf(fp,"%lf\t",_ValFxyz[i][j+1][k+1]);  fprintf(fp,"%lf\n",_ValFxyz[i+1][j+1][k+1]);        
+        fprintf(fp,"%.15e\t",_ValFxyz[i][j][k]);  fprintf(fp,"%.15e\t",_ValFxyz[i+1][j][k]);
+        fprintf(fp,"%.15e\t",_ValFxyz[i][j+1][k]);  fprintf(fp,"%.15e\t",_ValFxyz[i+1][j+1][k]);
+        fprintf(fp,"%.15e\t",_ValFxyz[i][j][k+1]);  fprintf(fp,"%.15e\t",_ValFxyz[i+1][j][k+1]);
+        fprintf(fp,"%.15e\t",_ValFxyz[i][j+1][k+1]);  fprintf(fp,"%.15e\n",_ValFxyz[i+1][j+1][k+1]);        
       }
 	 
   fclose(fp);
